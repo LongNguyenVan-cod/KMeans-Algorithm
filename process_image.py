6c19b7df29cdac22f79ea842034ab6e8b4b2cd92
LongNguyenVan-cod <longnguyenvanfpf@gmail.com>
2025-06-04 23:07:38 +0700

    Update process_image.py

--- process_image.py
+++ process_image.py
@@ -9,27 +9,18 @@ image = plt.imread('a.jpg')
 width = image.shape[0]
 height = image.shape[1]
 
-print(image.shape)
-
 # Reshape mensional of image
-image = image.reshape(width*height, 3 )
-print(image.shape)
+image = image.reshape(width*height, 4)
 
 # Use algorithm
 kmeans = KMeans(n_clusters=4 ).fit(image)
-
-# Take labels and clusters
 labels = kmeans.predict(image)
 clusters = kmeans.cluster_centers_
-print(labels)
-print(clusters)
+# print(labels)
+# print(clusters)
 
 # Craete image_2 like mensional image after reshape
-# image_2 = numpy.zeros_like(image)
-image_2 = numpy.zeros((width,height,3 ), dtype=numpy.uint8)  #float32, uint8
-
-# for i in range(len(image_2)):
-# 	image_2[i] = clusters[labels[i]]
+image_2 = numpy.zeros((width,height,4), dtype=numpy.float32)  #float32, uint8
 
 index = 0
 for i in range(width):
@@ -37,8 +28,6 @@ for i in range(width):
 		labels_of_index = labels[index]
 		image_2[i][j] = clusters[labels_of_index]
 		index +=1
-
-# image_2 = image_2.reshape(width,height,3)
 
 plt.imshow(image_2)
 plt.show()
